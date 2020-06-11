5c782a717ba2ce00109bd6e0c76a2b151a415345
Jerry Guerra <Jerry Guera>
2020-06-10 21:22:42 -0600

    Changes to point to endpoint

--- handler.py
+++ handler.py
@@ -1,21 +1,40 @@
 import requests
-
-
-r = requests.get("https://reqres.in/api/users")
-r_dict = r.json()
+import json
 
 def post_endpoint(event, context):
+    r = requests.get("https://reqres.in/api/users")
+    r_dict = r.json()
     count = 0
-    inp = input('type a number:  ')
+    inp = event['body']
     num = int(inp)
     tim = 0
-    while (count < num ):
-        fname = r_dict['data'][tim]['first_name']
-        lname = r_dict['data'][tim]['last_name']
-        fullname = fname+" "+lname
-        print(fullname)
-        tim = tim +1
-        count = count +1
-    print("**************")
-    print("completed")
+    s_name = {}
+    list_of_names = []
+while (count < num ):
+        for i in num:
+            try:
+                fname = r_dict['data'][tim]['first_name']
+                lname = r_dict['data'][tim]['last_name']
+                fullname = fname+" "+lname
+                s_name = {'fullname' : fullname}
+                list_of_names.append(s_name)
+                print(fullname)
+                tim = tim +1
+                count = count +1
+    #print("**************")
+    #print("completed")
+            except:
+                response = {
+                    'statusCode' : 401,
+                    'response' : 'Cant reach endpoint'
+                }
+        response = {
+            'statusCode' : 200,
+            'response': list_of_names
+        }
 
+        r_json = {
+            'body' : json.dumps(response)
+        }
+
+        return r_json
